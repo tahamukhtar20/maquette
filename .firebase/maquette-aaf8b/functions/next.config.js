cb06177f9aa56ae0a0881b2593c52fa7de6798c7
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-08-03 12:45:24 +0500

    firebase local deployment and fuck github workflows

---
+++ .firebase/maquette-aaf8b/functions/next.config.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// next.config.js
+var nextConfig = {
+  images: {
+    domains: [
+      "picsum.photos",
+      "images.unsplash.com",
+      "cdn.sanity.io",
+      "localhost"
+    ]
+  }
+};
+module.exports = nextConfig;
