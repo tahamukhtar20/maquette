5b2ca37f948f6715d85e3a966a9e68153219027e
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-08-10 13:26:45 +0500

    added loading states

---
+++ src/app/edit-profile/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <main className="min-h-screen w-full grid place-items-center">
+      <span className="loading loading-infinity loading-lg text-primary"></span>
+    </main>
+  );
+}
